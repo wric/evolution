fb2f7d052632c789f61e424777c328dabddb8de3
Richard Westberg <westberg.richard@gmail.com>
2021-01-04 20:13:25 +0100

    change printer to websockets

--- evolution/printer.py
+++ evolution/printer.py
@@ -1,31 +1,27 @@
-import zmq
+# Simple printer utility to listen to all websocket communication.
+# Based on client example from: https://websockets.readthedocs.io/en/stable/intro.html
 
-from evolution.utils import worker, worker_main_loop
+import asyncio
+import json
+import sys
 
-
-@worker
-def printer(ports):
-    context = zmq.Context()
-    subscriber = context.socket(zmq.SUB)
-
-    for port in ports:
-        subscriber.connect(f"tcp://127.0.0.1:{port}")
-
-    subscriber.subscribe("")
-
-    @worker_main_loop("printer", None)
-    def main_loop():
-        while True:
-            message = subscriber.recv_string()
-            topic, value = message.split(" ", 1)
-            print(f"{topic} > {value}")
-
-    main_loop()
+import websockets
 
 
-def main():
-    printer([5555, 5556, 5557])
+async def printer(uri, topics=[]):
+    async with websockets.connect(uri) as websocket:
+        async for message in websocket:
+            data = json.loads(message)
+
+            if topics and data["type"] not in topics:
+                return
+
+            print(data)
 
 
 if __name__ == "__main__":
-    main()
+    uri = "127.0.0.1:6789" if len(sys.argv) == 0 else sys.argv[0]
+    topics = [] if len(sys.argv) < 2 else sys.argv[1].split(",")
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(printer(uri, topics))
